e932cc2edfa28e6227a6a30efa1fb248f1d73e34
Max Schaufler <bytedoc@bytedoc.net>
2016-11-09 17:33:17 +0100

    Core plugin files and folders, without implementation of .js or .java parts

---
+++ src/android/SamsungKnoxSSO.java
@@ -0,0 +1,13 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.*;
+
+// code will come here ...
