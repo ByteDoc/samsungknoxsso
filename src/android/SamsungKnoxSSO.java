96846212d3f34fb274123297fb515b2213d6bce2
Max Schaufler <bytedoc@bytedoc.net>
2016-11-10 00:13:01 +0100

    inserted correct sso.jar, implemented .java and .js,, no builds yet

--- src/android/SamsungKnoxSSO.java
+++ src/android/SamsungKnoxSSO.java
@@ -7,7 +7,77 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.sec.android.service.authentication.*;
+import com.sec.android.service.singlesignon.aidls.*;
+
 
 import java.util.*;
 
-// code will come here ...
+public class SamsungKnoxSSO extends CordovaPlugin {
+
+	private JSONArray argsArray;
+	private JSONObject argsObject;
+	private CallbackContext callbackContext;
+	private String authenticationDomain;
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext context) throws JSONException {
+		argsArray = args;
+		callbackContext = context;
+
+		// get all required parameters
+		try{
+			argsObject = argsArray.getJSONObject(0);
+			
+			//authenticationDomain = argsObject.getString("authenticationDomain");
+			authenticationDomain = "emm.samsungknox.com";
+		}catch(JSONException e){
+			callbackContext.error(e.getMessage());
+		}	
+
+		
+		if (action.equals("getToken")) {
+			getToken();
+		} else {
+			callbackContext.error("Unknow action '" + action + "' in plugin SamsungKnoxSSO");
+		}
+	}
+			
+	private void getToken() {
+		AsyncTask loadTask = new AsyncTask<Void, Void, Void>() {
+
+			@Override
+			protected String doInBackground(Void... params) {
+				SecurityToken securityToken = null;
+				String mRequestedServiceAccessToken = null;
+
+				try {
+					securityToken = EnterpriseAuthentication.getInstance(SamsungKnoxSSO.this).getSecurityToken(
+						SamsungKnoxSSO.this,
+						"HTTP@"+authenticationDomain,		// Service URL
+						SingleSignOnTokenType.KERBEROS
+					);
+					if ( securityToken != null ) {
+						//mRequestedServiceAccessToken can be sent in HTTP authorization header
+						mRequestedServiceAccessToken = securityToken.getNegotiateToken();
+						JSONObject cbObject = new JSONObject();
+						cbObject.put("token", mRequestedServiceAccessToken);
+						JSONArray cbArray = new JSONArray();
+						cbArray.put(cbOject);
+						callbackContext.success(cbArray);
+					} else {
+						callbackContext.error("Token received is null");
+					}
+				} catch (NotAuthenticatedException ex) {
+					callbackContext.error("Exception: [" + ex.getMessage()+"]");
+				} catch (SecurityProviderNotFoundException ex) {
+					callbackContext.error("Exception: [" + ex.getMessage()+"]");
+				} 
+				return null;
+			}
+		};
+		loadTask.execute();
+			
+	}
+	
+}
--- www/samsung-knox-sso.js
+++ www/samsung-knox-sso.js
@@ -1,3 +1,13 @@
+function getArgsArray(args) {
+	// args auf erlaubten typ/inhalt prüfen
+	// nur ein Object erlaubt, kein Array! 
+	if (typeof(args) != "object" || args == null || Array.isArray(args)) args = {};
+	return [args];	// Array erstellen
+}
+
 module.exports = {
-	// code will come here ...
+	getToken: function (args, successCallback, errorCallback) {
+		var argsArray = getArgsArray(args);
+		cordova.exec(successCallback, errorCallback, "SamsungKnoxSSO", "getToken", argsArray);
+	}
 };
