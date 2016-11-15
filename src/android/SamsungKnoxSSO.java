96f12d48faff3d8125263d2fedccb5a40574be8a
Max Schaufler <bytedoc@bytedoc.net>
2016-11-15 11:51:10 +0100

    corrected some literals

--- src/android/SamsungKnoxSSO.java
+++ src/android/SamsungKnoxSSO.java
@@ -112,16 +112,16 @@ public class SamsungKnoxSSO extends CordovaPlugin {
                     callbackContext.success(argsArray);
 
                 } else {
-                    logError("--- check --- "Token received is null");
+                    logError("--- check --- Token received is null");
                     callbackContext.error("Token received is null");
                 }
             } catch (NotAuthenticatedException ex) {
                 message = ex.getMessage();
-                logError("--- MainActivity -> GetTokenTask ---", "Exception: [" + message + "]");
+                logError("--- MainActivity -> GetTokenTask --- Exception: [" + message + "]");
                 callbackContext.error("--- MainActivity -> GetTokenTask --- Exception: [" + message + "]");
             } catch (SecurityProviderNotFoundException ex) {
                 message = ex.getMessage();
-                logError("--- MainActivity -> GetTokenTask ---", "Exception: [" + message + "]");
+                logError("--- MainActivity -> GetTokenTask --- Exception: [" + message + "]");
                 callbackContext.error("--- MainActivity -> GetTokenTask --- Exception: [" + message + "]");
             }
             return message;
